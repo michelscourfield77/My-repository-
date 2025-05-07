7076c1643f836fad6b202cf88ff45f4f67c97a59
michelscourfield77 <85682204+michelscourfield77@users.noreply.github.com>
2025-05-08 00:27:51 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# mon_calculateur
+
+Un petit module pour évaluer une expression mathématique et comparer un résultat donné.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mon_calculateur',
+    version='0.1',
+    packages=find_packages(),
+    description='Module local pour évaluer des expressions mathématiques',
+    author='TonNom',
+    python_requires='>=3.6',
+)
